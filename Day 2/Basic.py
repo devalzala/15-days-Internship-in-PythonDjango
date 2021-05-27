c14151e69788dc25fecc43c80eec8a0372b657b6
Devalsinh Zala <84832101+devalzala@users.noreply.github.com>
2021-05-27 12:43:22 +0530

    Add files via upload

---
+++ Day 2/Basic.py
@@ -0,0 +1,45 @@
+# Hash is used to comment in the Python
+''' Inverted comma are also used to comment in python '''
+""" Double Inverted comma are also used to comment in python """
+# VAriables
+
+n1 = 10
+n2 = 11
+name = 'devalsinh'
+print(n1)
+print(n2)
+print('My name is',name)
+
+# Datatypes
+n=10 + 2j
+print(type(n)) # It will give n as a complex type.
+
+n3=10
+print(type(n3)) # It will give n1 as a Integer type.
+
+#list
+#list are use to store all type of datatypes in one single array, and is also mutable.
+
+l1=[10,20,'deval'] #List are mutables that means there values can be changed
+
+print(l1)
+print(type(l1))
+
+# Tuple
+# Tuple are also use to store all type of datatypes like list but the difference is it is immutable.
+
+tuple = (1,2,3,'Hi there') #Tuples are immutables that means there values cannnot be changed
+print(tuple)
+# Dictonaries 
+
+d = {1: 'Deval', 2: 'Zala'}
+print(d[1],d[2])
+
+# Split/Slice
+
+Name = "Devalsinh"
+print(Name[0:3])
+print(Name[0:])
+print(Name[ :6])
+
+
