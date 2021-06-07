5198f3595cee8dfd5864acee4ab1d8d155353aa3
Devalsinh Zala <84832101+devalzala@users.noreply.github.com>
2021-06-07 15:08:08 +0530

    Day 4 in Django Creating Form..

---
+++ Day 9/Task/myapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def homepageview(request):
+    return render(request,"home.html",{})
+
+def aboutpageview(request):
+    return render(request,"about.html")
+
+def contactpageview(request):
+    return render(request,"contact.html")
+
+def myform(request):
+    return render(request,"myform.html")
+
+def process(request):
+    print("Welcome")
+    print(request.method)
+    print(request.POST)
+    name  = (request.POST['name'])
+    email  = (request.POST['Email'])
+    age = int(request.POST['age'])
+    contact = int(request.POST['contact'])
+    city = (request.POST['city'])
+    print(name, email, age, contact, city)
+    
+    return render(request,"ans.html",{
+            'myname':name,
+            'myemail':email,
+            'myage':age,
+            'mycontact':contact,
+            'mycity':city,})
